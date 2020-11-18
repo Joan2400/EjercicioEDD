214599dc26865d544a88eca157ee7fed0c43cd92
Joanpe2400 <joanpe2400@gmail.com>
2020-11-18 11:03:39 +0100

    Mi primer commit

---
+++ p362_Navidad.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class p362_Navidad {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int dia, mes;
+		
+		dia = sc.nextInt();
+		mes = sc.nextInt();
+		
+		while (dia < 32 && mes < 13) {
+			
+		}
+		
+
+	}
+
+}
